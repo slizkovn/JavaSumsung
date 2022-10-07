b283014a95ab6e86f28ab0f7acf22d5440750494
slizkovn <nikslizkov@gmail.com>
2022-10-07 11:45:38 +0300

    first prog ready

--- Prog1.java
+++ Prog1.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class Prog1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+
+        System.out.printf("%d", num1 + num2);
+        in.close();
     }
 }

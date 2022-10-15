f0d247315a99f0a4b6ec96782a089f5a4b5ff53d
slizkovn <nikslizkov@gmail.com>
2022-10-15 10:39:26 +0300

    new file

--- Prog1.java
+++ Prog1.java
@@ -1,14 +1,8 @@
 import java.util.Scanner;
 
 public class Prog1 {
-
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
-
-        System.out.printf("%d", num1 + num2);
-        in.close();
+        System.out.println(in.nextInt() % 10);
     }
 }
